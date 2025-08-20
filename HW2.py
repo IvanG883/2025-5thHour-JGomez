8d4f93cd3eef7fa736f206152fa11c390123f01f
Ivan Gomez <jg125092@student.wpsok.org>
2025-08-20 12:52:45 -0500

    finished HW2

---
+++ HW2.py
@@ -0,0 +1,33 @@
+#Name: Ivan Gomez
+#Class: 5th Hour
+#Assignment: HW2
+
+
+#1. Print "Hello World!"
+print(" hello world")
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+number= 7
+string= "hello!"
+isIvan= True
+
+#3. Print all three variables on the same print function (at the same time).
+print(number,string,isIvan)
+
+
+#4. Create a variable that asks the user to input an integer.
+firstnumber = int(input("enter first number"))
+
+
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+sum= firstnumber + number
+print(sum)
+#6. Take the result from #5 and divide it by 2. Print the result.
+print(sum/2)
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+isIvan = False
+print(isIvan)
+#8. Print the value of the boolean variable.
+#9. Create a variable with a number that contains decimals.
+numberdec = 2.5
+#10. Round the number from #9 up or down using the round function.
+print(round(numberdec))
