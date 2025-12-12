f5ddd8cca701a00e30626ee0433750eda002b44a
Hogan McCune <hm127038@student.wpsok.org>
2025-12-12 13:20:02 -0600

    worked on the semester test

---
+++ Semester Test.py
@@ -0,0 +1,142 @@
+#Name: Ivan Gomez
+#Class: 5th Hour
+#Assignment: Semester Project 1
+
+import random
+import time
+
+#Due to weird time travelling circumstances beyond explanation, you find yourself in 2018 or so
+#working for Larian Studios. Currently, they are working on the early prototypes of the hype
+#upcoming game "Baldur's Gate 3". BG3 is a game that uses the Dungeons & Dragons 5th edition rules
+#as its framework for gameplay. You have been given a basic dictionary of some of the main hero
+#characters and some of the enemies they may face, and are tasked with making an early prototype
+#of one of the party members fighting against an enemy until one of them hits zero HP (dies).
+
+partyDict = {
+    "LaeZel" : {
+        "HO" : 48,
+        "Init" : 1,
+        "AC" : 17,
+        "AtkMod": 6,
+        "Damage" : random.randint(1,6) + random.randint(1,6) + 3
+    },
+    "Shadowheart" : {
+        "HP" : 40,
+        "Init" : 1,
+        "AC" : 18,
+        "AtkMod": 4,
+        "Damage" : random.randint(1,6) + 3,
+    },
+    "Gale" : {
+        "HP" : 32,
+        "Init" : 1,
+        "AC" : 14,
+        "AtkMod": 6,
+        "Damage" : random.randint(1,10) + random.randint(1,10),
+    },
+    "Astarion" : {
+        "HP" : 40,
+        "Init" : 3,
+        "AC" : 14,
+        "AtkMod": 5,
+        "Damage" : random.randint(1,8) + random.randint(1,6) + 4,
+    }
+}
+
+enemyDict = {
+    "Goblin" : {
+        "HP" : 7,
+        "Init" : 0,
+        "AC" : 12,
+        "AtkMod": 4,
+        "Damage" : random.randint(1,6) + 2
+    },
+    "Orc": {
+        "HP" : 15,
+        "Init": 1,
+        "AC" : 13,
+        "AtkMod": 5,
+        "Damage" : random.randint(1,12) + 3
+    },
+    "Troll": {
+        "HP" : 84,
+        "Init": 1,
+        "AC" : 15,
+        "AtkMod": 7,
+        "Damage" : random.randint(1,6) + random.randint(1,6) + 4
+    },
+    "Mindflayer": {
+        "HP" : 71,
+        "Init": 1,
+        "AC" : 15,
+        "AtkMod": 7,
+        "Damage" : random.randint(1,10) + random.randint(1,10) + 4
+    },
+    "Dragon": {
+        "HP" : 127,
+        "Init": 2,
+        "AC" : 18,
+        "AtkMod": 7,
+        "Damage" : random.randint(1,10) + random.randint(1,10) + random.randint(1,8) + 4
+    },
+}
+
+#Combat consists of these steps:
+
+#1. Rolling for 'initiative' to see who goes first. This is determined by rolling a
+#20-sided die (d20) and adding their initiative modifier (If the roll is the same,
+#assume the hero goes first).
+Player1 = random.randint(1,20)
+player2 = random.randint(1,20)
+if Player1 >=  player2:
+    print("shadowheart goes first")
+    Player1first = True
+else :
+    print("Orc goes first")
+    Player1first = False
+if Player1first == True:
+    print("Shadowheart goes first")
+else :
+    print("Orc goes first")
+#2. Rolling to attack. This is determined by rolling a 20-sided die (d20) and adding their
+#attack modifier. The attack hits if it matches or is higher than the target's Armor Class (AC).
+#If the d20 rolled to attack is an unmodified ("natural") 20, the attack automatically hits and
+#the character deals double damage. If the d20 rolled to attack is an unmodified ("natural") 1,
+#the attack automatically misses
+while partyDict["Shadowheart"]["HP"] > 0 and enemyDict["Orc"]["HP"] > 0:
+    if Player1first == True:
+        print("Shadowheart goes first")
+        Player1atk =random.randint(1,20)
+        print(Player1atk)
+        if Player1atk == 20:
+            enemyDict["Orc"]["HP"] -= (partyDict["Shadowheart"]["Damage"]*2)
+            print(f"Shadowheart hits Orc for {(partyDict["Shadowheart"]["Damage"]*2)}")
+        elif Player1atk == 1:
+            print("Roll natural 1, Missed attack")
+        elif Player1atk + partyDict["Shadowheart"]["AtkMod"] >= enemyDict["Orc"]["AC"]:
+            enemyDict["Orc"]["HP"] -= (partyDict["Shadowheart"]["Damage"])
+            print(f"Shadowheart hits Orc for {(partyDict["Shadowheart"]["Damage"])}")
+        else:
+           print("Shadowheart missed thir attack")
+
+
+       Player2atk =random.randint(1,20)
+    print(Player2atk)
+       if Player2atk == 20:
+            partyDict["Shadowheart"]["HP"] -= (enemyDict["Orc"]["Damage"]*2)
+            print(f"Orc hits Shadowheart for {(partyDict["Orc"]["Damage"]*2)}")
+        elif Player2atk == 1:
+            print("Roll natural 1, Missed attack")
+        elif Player2atk + enemyDict["Orc"]["AtkMod"] >= partyDict["Shadowheart"]["AC"]:
+            partyDict["Shadowheart"]["HP"] -= (enemyDict["Orc"]["Damage"])
+            print(f"Orc hits Orc for {(enemyDict["Orc"]["Damage"])}")
+        else:
+            print("Orc missed thir attack")
+
+
+
+#3. If the attack hits, roll damage and subtract it from the target's hit points.
+
+#4. The second in initiative rolls to attack (and rolls damage) afterwards.
+
+#5. Repeat steps 2-5 until one of the characters is dead.
